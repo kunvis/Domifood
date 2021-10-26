8b1d554afe029ce136a4585dc33ce905d82ccb54
Kunvis <88859574+kunvis@users.noreply.github.com>
2021-10-25 21:26:41 -0500

    Sprint 1 Modelado

---
+++ Sprint 1/DomiciliosRappidin/inicio.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOMICILIOS RAPPIDIN</title>
+    <link rel="stylesheet" href="inicio.css">
+</head>
+<body>
+    <header>
+        <img src="imagenes/logo.jpeg" id="logo" width="100px"><a href="http://localhost/formacion/DomiciliosRappidin/inicio.php">
+            <h1>DOMICILIOS RAPPIDIN</h1></a>
+    </header>
+    <br>
+    <nav>
+        <li><a id="registra" href="http://localhost/formacion/DomiciliosRappidin/registrate/registro.php">Registrarme</a></li>
+        <li><a id="mision" href="http://localhost/formacion/DomiciliosRappidin/nosotros/misionvision.php">Nosotros</a></li>
+        <li><a id="ingreso" href="http://localhost/formacion/DomiciliosRappidin/ingreso/ingreso.php">Iniciar Sesión</a></li>
+    </nav>
+    <br>
+    <div class="info1">
+        <img src="imagenes/domira.jpg" width="170px">
+        <p id="descrip"> Es una app donde cualquier persona puede encontrar cierta variedad 
+            de restaurantes que se encuentra en el municipio de Soacha (Cundinamarca) y a sus alrededores, 
+            cada uno de ellos tiene su propio menú que se puede ajustar al gusto y  presupuesto 
+            del cliente para realizar su pedido y entregar por el medio domiciliario</p>
+    </div>
+    <br>
+    <div class="info2">
+        <strong><p id="titulo">AHORRA TIEMPO</p></strong>
+        <img src="imagenes/reloj.gif" width="130px">
+        <p id="descrip">Recibe tu pedido en menos de 1 hora. <p>
+    </div>
+    <br>
+    <div class="info3">
+        <strong><p id="titulo">EN MANOS EXPERTAS</p></strong>
+        <img src="imagenes/atencion.jpg" width="130px">
+        <p id="descrip">Atención total al cliente durante, antes y después de la compra.<p>
+    </div>
+    <br>
+    <div class="info4">
+        <strong><p id="titulo">CALIDAD 100%</p></strong>
+        <img src="imagenes/iso.png" width="150px">
+        <p id="descrip">Brindamos el mejor servicio de calidad a cualquier persona, 
+            haciéndolos sentir confiados y seguros al realizar sus pedidos.<p>
+    </div>
+    <br>
+    <div class="app">
+        <strong><p id="descrip">Disfruta una mejor experiencia descargando nuestra app en tu teléfono movil</p></strong>    
+        <img src="imagenes/descarga.jpg" width="130px">
+        <a id="boton" href="https://play.google.com/"><strong><p>CLICK AQUI</p></strong></a>
+    </div>
+</body>
+<footer>
+    Síguenos en
+    <ul><img src="imagenes/facebook.png" width="25px">DomiciliosRappidin</ul>
+    <ul><img src="imagenes/instagram.png" width="25px">DomiciliosRappidin</ul>
+    <ul><img src="imagenes/twitter.png" width="25px">DomiciliosRappidin</ul>
+</footer>
+</html>
---
+++ Sprint 1/DomiciliosRappidin/vista/restaurantes.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+    $usuario1=($_SESSION['user']);
+    echo "<strong>Bienvenid@...$usuario1</strong>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOMICILIOS RAPPIDIN</title>
+    <link rel="stylesheet" href="../vista/estilo/restaurantes.css">
+</head>
+<body>
+    <header>
+        <img src="imagenes/logo.jpeg" id="logo" width="100px"><a href="http://localhost/formacion/DomiciliosRappidin/inicio.php">
+        <h1>DOMICILIOS RAPPIDIN</h1></a>
+    </header>
+    <h2>Restaurantes</h2>
+    <br>
+    <article class="uno">
+        <section class="gato">
+            <h3>EL GATO</h3><br>
+            <img src="imagenes/gato.jpg">
+            <br><br>
+            <p id="clasegato">Más Información <a href="local_1.html">Click aquí</a></p>
+        </section>
+        <br>
+        <section class="tabla">
+            <h3>LA TABLA</h3><br>
+            <br><br><br>
+            <img src="imagenes/tabla.JPG">
+            <br><br><br>
+            <p id="clasetabla">Más Información <a href="local_2.html">Click aquí</a></p>
+        </section>
+        <br>
+        <section class="piolin">
+            <h3>EL PIOLIN</h3><br>
+            <img src="imagenes/piolin.JPG">
+            <br><br><br>
+            <p id="clasepiolin">Más Información <a href="local_3.html">Click aquí</a></p>
+        </section>
+        <br>
+    </article>
+    <br>
+    <article class="dos">
+        <section class="vikingos">
+            <h3>VIKINGOS</h3><br>
+            <img src="imagenes/vikingos.JPG">
+            <br><br>
+            <p id="clasevikingos">Más Información <a href="local_4.html">Click aquí</a></p>
+        </section>
+        <br>
+        <section class="kfc">
+            <h3>KFC</h3><br>
+            <img src="imagenes/kfc.JPG">
+            <br><br>
+            <p id="clasekfc">Más Información <a href="local_5.html">Click aquí</a></p>
+        </section>
+        <br>
+        <section class="tio">
+            <h3>EL TIO RICO</h3><br>
+            <br><br><br>
+            <img src="imagenes/tio.JPG">
+            <br><br>
+            <br>
+            <p id="clasetio">Más Información <a href="local_6.html">Click aquí</a></p>
+        </section>
+    </article>
+    <br>
+    <form method="POST" action="../modelo/cierre.php">
+        <input type="submit" name="cierre" value="Cerrar Sesión">
+    </form>
+    </body>
+<footer>
+    Síguenos en
+    <ul><img src="../imagenes/facebook.png" width="25px">DomiciliosRappidin</ul>
+    <ul><img src="../imagenes/instagram.png" width="25px">DomiciliosRappidin</ul>
+    <ul><img src="../imagenes/twitter.png" width="25px">DomiciliosRappidin</ul>
+</footer>
+</html>
